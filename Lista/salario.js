f97ce5a3b3040a10523266ae226e4bbf740da349
Maria_ed_pedroso <maria_ed_pedroso@estudante.sesisenai.org.br>
2024-05-14 09:08:20 -0300

    quase

---
+++ Lista/salario.js
@@ -0,0 +1,20 @@
+function salario(){
+   
+    let nome = prompt('Qual o seu nome?');
+   
+    let salarioBruto= 0;
+
+    salarioBruto = parseFloat(window.prompt('Informe seu Salario Bruto'));
+
+    let valorINSS = 0.08;
+
+    let desconto=0;
+   
+    desconto=salarioBruto*valorINSS;
+
+    salarioLiquido = salarioBruto - desconto
+
+    window.alert("funcionario: " + nome)
+    window.alert("Com o desconto, terá o salario liquido de:" + salarioLiquido)
+
+}
--- Lista/variavel.js
+++ Lista/variavel.js
@@ -1,6 +1,5 @@
 function variavel() {
     let valor1 = parseInt(document.getElementById("valor1").value);
     let valor2 = parseInt(document.getElementById("valor2").value);
-
-
+    let di = 0
 }
